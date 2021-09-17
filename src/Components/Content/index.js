6a0caa598983f0a90f59cf0400cb7e51c2e48648
Juan Hernandez <juanh22@gmail.com>
2021-09-17 17:09:22 -0600

    first content on page after some errors

---
+++ src/Components/Content/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Content() {
+
+    return (
+        <section> 
+            <p> Hi There Friend! </p>
+        </section>
+    );
+}
+
+export default Content;
